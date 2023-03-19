c3271323f3d6f1425139352b365c710a512da68e
Michael Dreisinger <mike.dreisinger95@gmail.com>
2023-03-18 21:24:09 -0600

    add VPC TF

---
+++ terraform/VPC/main.tf
@@ -0,0 +1,11 @@
+# aws_vpc.default:
+resource "aws_vpc" "default" {
+    assign_generated_ipv6_cidr_block     = false
+    cidr_block                           = "172.31.0.0/16"
+    enable_dns_hostnames                 = true
+    enable_dns_support                   = true
+    enable_network_address_usage_metrics = false
+    instance_tenancy                     = "default"
+    tags                                 = {}
+    tags_all                             = {}
+}
