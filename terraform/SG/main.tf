3b7e916a6993bcbaa5e6b02da2ae1cff93572f3c
Michael Dreisinger <mike.dreisinger95@gmail.com>
2023-03-18 21:24:28 -0600

    add SG TF

---
+++ terraform/SG/main.tf
@@ -0,0 +1,49 @@
+# aws_security_group.default:
+resource "aws_security_group" "default" {
+    description = "default VPC security group"
+    egress      = [
+        {
+            cidr_blocks      = [
+                "0.0.0.0/0",
+            ]
+            description      = ""
+            from_port        = 0
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "-1"
+            security_groups  = []
+            self             = false
+            to_port          = 0
+        },
+    ]
+    ingress     = [
+        {
+            cidr_blocks      = [
+                "172.31.16.203/32",
+            ]
+            description      = "Allow bastion traffic to DB"
+            from_port        = 3306
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "tcp"
+            security_groups  = []
+            self             = false
+            to_port          = 3306
+        },
+        {
+            cidr_blocks      = []
+            description      = ""
+            from_port        = 0
+            ipv6_cidr_blocks = []
+            prefix_list_ids  = []
+            protocol         = "-1"
+            security_groups  = []
+            self             = true
+            to_port          = 0
+        },
+    ]
+    name        = "default"
+    tags        = {}
+    tags_all    = {}
+    vpc_id      = "vpc-04f96020d6b2e8308"
+}
