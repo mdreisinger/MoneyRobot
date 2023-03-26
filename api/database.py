c70aac8571b5a619cb11f096ce55e1fe178e43e7
Michael Dreisinger <mike.dreisinger95@gmail.com>
2023-03-26 16:41:38 -0600

    retry connecting to db on exception

--- api/database.py
+++ api/database.py
@@ -12,7 +12,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# pylint:disable=relative-beyond-top-level
 from .db_info import get_db_info
 
 
@@ -20,7 +19,14 @@ rds_host, username, password, db_name = get_db_info()
 
 SQLALCHEMY_DATABASE_URL = f"mysql://{username}:{password}@{rds_host}:3306/{db_name}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+def create_engine_session():
+    """
+    Simple function to create engine session.
+    Useful if API drops connection to DB, this can be called. 
+    """
+    return create_engine(SQLALCHEMY_DATABASE_URL), \
+           sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+engine, SessionLocal = create_engine_session()
 
 Base = declarative_base()
--- api/main.py
+++ api/main.py
@@ -2,16 +2,17 @@
 main.py
 """
 
-from typing import List
-
 from fastapi import Depends, FastAPI
+import MySQLdb
 from sqlalchemy.orm import Session
 
 from . import crud
-from .database import SessionLocal, engine
+from .database import create_engine_session
 from .models import Base
 from .schemas import transaction
 
+
+engine, SessionLocal = create_engine_session()
 
 Base.metadata.create_all(bind=engine)
 
@@ -22,25 +23,33 @@ def get_db():
     """
     Get db dependecy
     """
-    db = SessionLocal()
+    _, db = create_engine_session()
     try:
         yield db
     finally:
+        #pylint:disable=no-member
         db.close()
 
 @app.post("/add_transaction/", response_model=transaction)
-def post_transaction(new_transaction: transaction, db: Session = Depends(get_db)):
+def add_transaction(new_transaction: transaction, db: Session = Depends(get_db)):
     """
     Add new transaction to table.
     """
-    return crud.add_transaction(db, new_transaction)
+    try:
+        return crud.add_transaction(db, new_transaction)
+    except MySQLdb.OperationalError as error:
+        print(f"Caught MySQLdb exception: {error}")
+        print("Attempting to reconnect to DB and re-try operation")
+        db = get_db()
+        return crud.add_transaction(db, new_transaction)
 
-@app.get("/get_transaction_by_id/{transaction_id}", response_model=List[transaction])
-def get_transaction_by_id(transaction_id: int, db: Session = Depends(get_db)):
-    """
-    Get a transaction given a transaction_id.
-    """
-    return crud.get_transaction_by_id(db, transaction_id)
+
+# @app.get("/get_transaction_by_id/{transaction_id}", response_model=List[transaction])
+# def get_transaction_by_id(transaction_id: int, db: Session = Depends(get_db)):
+#     """
+#     Get a transaction given a transaction_id.
+#     """
+#     return crud.get_transaction_by_id(db, transaction_id)
 
 # @app.patch("/update_transaction/{transaction_id}", response_model=transaction)
 # def patch_transaction(transaction_id: int, db: Session = Depends(get_db)):
